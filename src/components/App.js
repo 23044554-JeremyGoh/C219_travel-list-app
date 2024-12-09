84092718116797cb3b453ad08ae566e4e5d08abf
23044554-JeremyGoh <23044554@myrp.edu.sg>
2024-12-10 03:54:55 +0800

    Fixed display list

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="app">
       <Logo />
-      <Form onAddItem={handleAddItems} handleClearItems={handleClearItems}/>
+      <Form onAddItems={handleAddItems} handleClearItems={handleClearItems}/>
       <PackingList items={items} 
       onDeleteItem={handleDeleteItem} 
       onUpdateItem={handleUpdateItem}/>
